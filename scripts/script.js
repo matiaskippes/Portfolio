c822c5a38387ed529657c0af173322e60b9b9fb4
Matias Kippes <matiaskippes99@gmail.com>
2021-11-05 17:21:41 -0300

    Proyect Base

---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+menu = document.getElementById('menu')
+header = document.querySelector('header')
+navbar = document.querySelectorAll('.navbar__item')
+form = document.querySelector('form')
+data = document.querySelectorAll('input.box, textarea.box')
+btn = document.querySelector('.submit')
+covid = document.getElementById('project_covid')
+cashier = document.getElementById('project_cashier')
+searcher = document.getElementById('project_searcher')
+finance = document.getElementById('project_finance')
+inventory = document.getElementById('project_inventory')
+store = document.getElementById('project_store')
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times')
+    header.classList.toggle('toggle')
+})
+navbar.forEach(btn => {
+    btn.addEventListener('click', () => {
+        menu.classList.remove('fa-times')
+        header.classList.remove('toggle')
+    })
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
+
+btn.addEventListener('click', (e) => {
+    data.forEach(register => {
+        console.log(register.value)
+        register.value = ""
+    })
+})
+
+covid.addEventListener('click', (e) => {
+    location.href = "https://github.com/matiaskippes/covid-analytics"
+})
+
+cashier.addEventListener('click', (e) => {
+    location.href = "https://github.com/matiaskippes/CajeroAutomatico"
+})
+
+searcher.addEventListener('click', (e) => {
+    location.href = "https://github.com/matiaskippes/TRZearcher"
+})
+
+finance.addEventListener('click', (e) => {
+    location.href = "https://github.com/matiaskippes/Finance-Firebase"
+})
+
+inventory.addEventListener('click', (e) => {
+    location.href = "https://github.com/matiaskippes/REST-API-Market"
+})
+
+store.addEventListener('click', (e) => {
+    location.href = "https://colinshop.netlify.app/index"
+})
